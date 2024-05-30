45848b6034b21f7c16fc9662f1bd2fd8f3af0dd3
ciny <ciy5364612@gmail.com>
2024-05-30 21:38:09 +0900

    7-3. Form 으로 입력받기

--- src/main/java/bootEx/demo/src/main/java/com/example/demo/section07/RequestParamController.java
+++ src/main/java/bootEx/demo/src/main/java/com/example/demo/section07/RequestParamController.java
@@ -1,13 +1,8 @@
 package com.example.demo.section07;
 
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.time.LocalDate;
 
 @Controller
 public class RequestParamController {
@@ -16,14 +11,19 @@ public class RequestParamController {
         return "entry";
     }
 
-    @PostMapping("confirm")
-    public String confirmView(Model model, @RequestParam String name,
-                              @RequestParam Integer age,
-                              @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam LocalDate birth) {
-        model.addAttribute("name", name);
-        model.addAttribute("age", age);
-        model.addAttribute("birth", birth);
+//    @PostMapping("confirm")
+//    public String confirmView(Model model, @RequestParam String name,
+//                              @RequestParam Integer age,
+//                              @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam LocalDate birth) {
+//        model.addAttribute("name", name);
+//        model.addAttribute("age", age);
+//        model.addAttribute("birth", birth);
+//
+//        return "confirm";
+//    }
 
-        return "confirm";
+    @PostMapping("confirm")
+    public String confirmView(Form form) {
+        return "confirm2";
     }
 }
