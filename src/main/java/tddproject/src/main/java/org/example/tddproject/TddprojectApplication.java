8448f235ca998d34d64f02f74335d832ff7ef32b
ciny <ciy5364612@gmail.com>
2024-06-13 21:45:04 +0900

    Tdd 연승용 프로젝트 setup

---
+++ src/main/java/tddproject/src/main/java/org/example/tddproject/TddprojectApplication.java
@@ -0,0 +1,13 @@
+package org.example.tddproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddprojectApplication.class, args);
+    }
+
+}
